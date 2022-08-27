4c2e9bc8c03dfbad79fe262caca8ef1fc1186c79
HaotianZhang <106578915+HaotianZhangAI4Science@users.noreply.github.com>
2022-08-27 15:35:50 +0800

    Create model_z00.py

---
+++ model_z00.py
@@ -0,0 +1,51 @@
+from torch import nn
+import torch
+
+class MultiLayerPerceptron(nn.Module):
+    """
+    Multi-layer Perceptron.
+
+    Note there is no activation or dropout in the last layer.
+
+    Parameters:
+        input_dim (int): input dimension
+        hidden_dim (list of int): hidden dimensions
+        activation (str or function, optional): activation function
+        dropout (float, optional): dropout rate
+    """
+
+    def __init__(self, input_dim, hidden_dims, activation="relu", dropout=0):
+        super(MultiLayerPerceptron, self).__init__()
+
+        self.dims = [input_dim] + hidden_dims
+        if isinstance(activation, str):
+            self.activation = getattr(F, activation)
+        else:
+            self.activation = None
+        if dropout:
+            self.dropout = nn.Dropout(dropout)
+        else:
+            self.dropout = None
+
+        self.layers = nn.ModuleList()
+        for i in range(len(self.dims) - 1):
+            self.layers.append(nn.Linear(self.dims[i], self.dims[i + 1]))
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        for i, layer in enumerate(self.layers):
+            nn.init.xavier_uniform_(layer.weight)
+            nn.init.constant_(layer.bias, 0.)
+
+    def forward(self, input):
+        """"""
+        x = input
+        for i, layer in enumerate(self.layers):
+            x = layer(x)
+            if i < len(self.layers) - 1:
+                if self.activation:
+                    x = self.activation(x)
+                if self.dropout:
+                    x = self.dropout(x)
+        return x
