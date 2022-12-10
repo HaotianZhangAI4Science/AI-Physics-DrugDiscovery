f5a0d9fd3f032539edc67cc2a590a68ad5ab39e8
HaotianZhang <106578915+HaotianZhangAI4Science@users.noreply.github.com>
2022-12-10 14:39:56 +0800

    Update surface_maker.py

--- surface_maker.py
+++ surface_maker.py
@@ -1,5 +1,6 @@
 pymesh documentation
 https://pymesh.readthedocs.io/en/latest/
-
+Note: the requirement of python for installing pymesh is >= 3.6 <3.7 (...It's wiered)
+  
 APBS and pdb2pqr software and documentation 
 https://www.poissonboltzmann.org/
