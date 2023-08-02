70c5764d3d9fdd9d3222357f02e8f916d85eee47
HaotianZhang <106578915+HaotianZhangAI4Science@users.noreply.github.com>
2023-08-02 22:47:41 +0800

    update

--- rdkit_blog/torsion.py
+++ rdkit_blog/torsion.py
@@ -68,5 +68,62 @@ def print_torsions(mol):
     nonring, ring = TorsionFingerprints.CalculateTorsionLists(mol)
     conf = mol.GetConformer(id=0)
     tups = [atoms[0] for atoms, ang in nonring]
+
     degs = [Chem.rdMolTransforms.GetDihedralDeg(conf, *tup) for tup in tups]
     print(degs)
+
+def get_torsion(mol):
+    conf = mol.GetConformer(id=0)
+    nonring, ring = TorsionFingerprints.CalculateTorsionLists(mol)
+    tups = [atoms[0] for atoms, ang in nonring]
+    degs = [Chem.rdMolTransforms.GetDihedralDeg(conf, *tup) for tup in tups]
+    qudra_types = [''.join([mol.GetAtomWithIdx(i).GetSymbol() for i in tup]) for tup in tups]
+    qudra_types_rever = [''.join([mol.GetAtomWithIdx(i).GetSymbol() for i in tup][::-1]) for tup in tups]
+    return degs+degs, qudra_types+qudra_types_rever
+
+def statis_torsion(mols):
+    
+    statis = {}
+    for mol in mols:
+        degs, qudra_types = get_torsion(mol)
+        for i, qudra_type in enumerate(qudra_types):
+            if qudra_type not in statis:
+                statis[qudra_type] = [degs[i]]
+            else:
+                statis[qudra_type].append(degs[i])
+    return statis
+
+from itertools import combinations
+
+def get_angles(mol):
+    angles = []
+    types = [] # double counting
+    conf =  mol.GetConformer(id=0)
+    for atom in mol.GetAtoms():
+        neighbors = [n.GetIdx() for n in atom.GetNeighbors()]
+        if len(neighbors) < 2:  # At least two neighbors are required to form an angle
+            continue
+        for a1, a2 in combinations(neighbors, 2):
+            angle = rdMolTransforms.GetAngleDeg(conf, a1, atom.GetIdx(), a2)
+            tup = (a1, atom.GetIdx(), a2)
+            type_ = [mol.GetAtomWithIdx(i).GetSymbol() for i in tup]
+            type = ''.join(type_)
+            type_rever = ''.join(type_[::-1])
+
+            angles.append(angle)
+            angles.append(angle)
+            types.append(type)
+            types.append(type_rever)
+    return types, angles
+
+def statis_angles(mols):
+
+    statis = {}
+    for mol in mols:
+        types, angles = get_angles(mol)
+        for i, tetra_type in enumerate(types):
+            if tetra_type not in statis:
+                statis[tetra_type] = [angles[i]]
+            else:
+                statis[tetra_type].append(angles[i])
+    return statis
